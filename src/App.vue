1b15c992795613b4cb37c3fd2f8896ccb2ffc3bf
Vladimir <v.lelicanin@dooapp.net>
2017-02-21 14:34:08 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="app">
+        <h1>{{ weather.main.temp | round }}</h1>
+    </div>
+</template>
+
+<script>
+  import axios from  'axios'
+  export default {
+    name: 'app',
+    data() {
+      return {
+        weather: {
+          main: {
+            temp: ""
+          }
+        }
+      }
+    },
+    filters: {
+      round: function (input) {
+        return Math.round(input)
+      },
+    },
+    created() {
+      let that = this;
+      axios.get('http://api.openweathermap.org/data/2.5/weather',
+          {
+            params: {
+              appid: 'b138aca5f679afcd22a1961e66aa5709',
+              q: 'Belgrade,RS',
+              units: 'metric'
+            }
+          }).then(function (response) {
+        that.weather = response.data
+        console.log(that.weather)
+      })
+    }
+
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import axios from 'axios'
+import App from './App.vue'
+
+
+new Vue({
+  el: '#app',
+  mounted(){
+
+  },
+  render: h => h(App)
+})
